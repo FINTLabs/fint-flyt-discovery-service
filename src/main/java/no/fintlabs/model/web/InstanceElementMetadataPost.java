45d2f4933c6fb579b69a5c4bd4d024dd42be5994
Egil Ballestad <egil.ballestad@vigoiks.no>
2023-01-06 15:02:44 +0100

    add NoArgsConstructor and AllArgsConstructor on models

--- src/main/java/no/fintlabs/model/web/InstanceElementMetadataPost.java
+++ src/main/java/no/fintlabs/model/web/InstanceElementMetadataPost.java
@@ -1,7 +1,9 @@
 package no.fintlabs.model.web;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import no.fintlabs.model.InstanceElementMetadata;
 
 import javax.validation.Valid;
@@ -11,6 +13,8 @@ import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class InstanceElementMetadataPost {
 
     private String key;
--- src/main/java/no/fintlabs/model/web/IntegrationMetadataPost.java
+++ src/main/java/no/fintlabs/model/web/IntegrationMetadataPost.java
@@ -1,7 +1,9 @@
 package no.fintlabs.model.web;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import no.fintlabs.validation.UniqueKeys;
 
 import javax.validation.Valid;
@@ -11,6 +13,8 @@ import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class IntegrationMetadataPost {
 
     @NotNull
