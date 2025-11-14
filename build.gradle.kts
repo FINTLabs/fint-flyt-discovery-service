c28f0aaf8e49bd1304884786c8ac9eecc868b48b
Egil Ballestad <egil.ballestad@vigoiks.no>
2025-11-14 08:53:47 +0100

    bump kafka and resource-server

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("java")
+    id("com.github.ben-manes.versions") version "0.53.0"
+}
+
+group = "no.fintlabs"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.named("annotationProcessor").get())
+    }
+}
+
+tasks.jar {
+    isEnabled = false
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.fintlabs.no/releases")
+    }
+    mavenLocal()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-core")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql")
+    runtimeOnly("org.postgresql:postgresql")
+    testRuntimeOnly("com.h2database:h2")
+
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("no.novari:kafka:5.0.0-rc-16")
+    implementation("no.novari:flyt-resource-server:6.0.0-rc-20")
+
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+
+    implementation("no.novari:flyt-resource-server:6.0.0-rc-20")
+
+    implementation("org.hibernate.validator:hibernate-validator")
+
+    implementation("org.apache.commons:commons-lang3:3.18.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/no/novari/IntegrationMetadataController.java
@@ -0,0 +1,130 @@
+package no.novari;
+
+
+import no.novari.flyt.resourceserver.security.user.UserAuthorizationService;
+import no.novari.model.dtos.InstanceMetadataContentDto;
+import no.novari.model.dtos.IntegrationMetadataDto;
+import no.novari.model.entities.IntegrationMetadata;
+import no.novari.validation.ValidationErrorsFormattingService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+
+import static no.novari.flyt.resourceserver.UrlPaths.INTERNAL_API;
+
+@RequestMapping(INTERNAL_API + "/metadata")
+@RestController
+public class IntegrationMetadataController {
+
+    private final IntegrationMetadataService integrationMetadataService;
+
+    private final Validator validator;
+    private final ValidationErrorsFormattingService validationErrorsFormattingService;
+    @Value("${novari.flyt.resource-server.user-permissions-consumer.enabled:false}")
+    private boolean userPermissionsConsumerEnabled;
+    private final UserAuthorizationService userAuthorizationService;
+
+    public IntegrationMetadataController(
+            IntegrationMetadataService integrationMetadataService,
+            Validator validator,
+            ValidationErrorsFormattingService validationErrorsFormattingService,
+            UserAuthorizationService userAuthorizationService
+    ) {
+        this.integrationMetadataService = integrationMetadataService;
+        this.validator = validator;
+        this.validationErrorsFormattingService = validationErrorsFormattingService;
+        this.userAuthorizationService = userAuthorizationService;
+    }
+
+    @GetMapping(params = {"kildeapplikasjonId", "kildeapplikasjonIntegrasjonId", "bareSisteVersjoner"})
+    public ResponseEntity<Collection<IntegrationMetadata>> getIntegrationMetadataForSourceApplication() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping(params = {"kildeapplikasjonId"})
+    public ResponseEntity<Collection<IntegrationMetadataDto>> getIntegrationMetadataForSourceApplication(
+            @AuthenticationPrincipal Authentication authentication,
+            @RequestParam(name = "kildeapplikasjonId") Long sourceApplicationId,
+            @RequestParam(name = "bareSisteVersjoner") Optional<Boolean> onlyLatestVersions
+    ) {
+        if (userPermissionsConsumerEnabled) {
+            userAuthorizationService.checkIfUserHasAccessToSourceApplication(authentication, sourceApplicationId);
+        }
+
+        Collection<IntegrationMetadataDto> integrationMetadata =
+                integrationMetadataService.getIntegrationMetadataForSourceApplication(
+                        sourceApplicationId,
+                        onlyLatestVersions.orElse(false)
+                );
+
+        return ResponseEntity.ok(integrationMetadata);
+    }
+
+    @GetMapping(params = {"kildeapplikasjonId", "kildeapplikasjonIntegrasjonId"})
+    public ResponseEntity<Collection<IntegrationMetadataDto>> getIntegrationMetadataForIntegration(
+            @AuthenticationPrincipal Authentication authentication,
+            @RequestParam(name = "kildeapplikasjonId") Long sourceApplicationId,
+            @RequestParam(name = "kildeapplikasjonIntegrasjonId") String sourceApplicationIntegrationId
+    ) {
+        if (userPermissionsConsumerEnabled) {
+            userAuthorizationService.checkIfUserHasAccessToSourceApplication(authentication, sourceApplicationId);
+        }
+
+        Collection<IntegrationMetadataDto> integrationMetadata =
+                integrationMetadataService.getAllForSourceApplicationIdAndSourceApplicationIntegrationId(
+                        sourceApplicationId,
+                        sourceApplicationIntegrationId
+                );
+        return ResponseEntity.ok(integrationMetadata);
+    }
+
+    @GetMapping("{metadataId}/instans-metadata")
+    public ResponseEntity<InstanceMetadataContentDto> getInstanceElementMetadataForIntegrationMetadataWithId(
+            @AuthenticationPrincipal Authentication authentication,
+            @PathVariable Long metadataId
+    ) {
+        IntegrationMetadataDto integrationMetadataDto = integrationMetadataService
+                .getById(metadataId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (userPermissionsConsumerEnabled) {
+            userAuthorizationService.checkIfUserHasAccessToSourceApplication(authentication, integrationMetadataDto.getSourceApplicationId());
+        }
+
+        return ResponseEntity.ok(integrationMetadataDto.getInstanceMetadata());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> post(
+            @AuthenticationPrincipal Authentication authentication,
+            @RequestBody IntegrationMetadataDto integrationMetadataDto
+    ) {
+        if (userPermissionsConsumerEnabled) {
+            userAuthorizationService.checkIfUserHasAccessToSourceApplication(authentication, integrationMetadataDto.getSourceApplicationId());
+        }
+
+        Set<ConstraintViolation<IntegrationMetadataDto>> constraintViolations = validator.validate(integrationMetadataDto);
+        if (!constraintViolations.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.UNPROCESSABLE_ENTITY,
+                    validationErrorsFormattingService.format(constraintViolations)
+            );
+        }
+        if (integrationMetadataService.versionExists(integrationMetadataDto)) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Version already exists");
+        }
+        integrationMetadataService.save(integrationMetadataDto);
+        return ResponseEntity.ok().build();
+    }
+
+}
