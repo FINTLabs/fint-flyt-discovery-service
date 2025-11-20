b452a73cac72bdfb85894c2b1019ad6f07c55e41
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2025-11-20 14:33:06 +0100

    Change namespace to `no.novari.flyt.discovery.service.*`

---
+++ src/main/java/no/novari/flyt/discovery/service/model/dtos/InstanceValueMetadataDto.java
@@ -0,0 +1,26 @@
+package no.novari.flyt.discovery.service.model.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
+import no.novari.flyt.discovery.service.model.entities.InstanceValueMetadata;
+
+@Getter
+@Builder
+@EqualsAndHashCode
+@Jacksonized
+public class InstanceValueMetadataDto {
+
+    @NotBlank
+    private final String displayName;
+
+    @NotNull
+    private final InstanceValueMetadata.Type type;
+
+    @NotNull
+    private final String key;
+
+}
---
+++ src/main/java/no/novari/flyt/discovery/service/model/entities/InstanceMetadataContent.java
@@ -0,0 +1,61 @@
+package no.novari.flyt.discovery.service.model.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.jackson.Jacksonized;
+
+import java.util.Collection;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Jacksonized
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class InstanceMetadataContent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    @Setter(AccessLevel.NONE)
+    private long id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private Collection<@Valid @NotNull InstanceValueMetadata> instanceValueMetadata;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private Collection<@Valid @NotNull InstanceObjectCollectionMetadata> instanceObjectCollectionMetadata;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private Collection<@Valid @NotNull InstanceMetadataCategory> categories;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InstanceMetadataContent that = (InstanceMetadataContent) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+}
